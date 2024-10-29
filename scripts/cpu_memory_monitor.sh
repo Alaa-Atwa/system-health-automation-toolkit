f6822b2694b95528093cf97d234d2d20e054ef7e
alaa atwa <alaa.atwa555@gmail.com>
2024-10-30 02:42:09 +0300

    cpu and ram usage monitor

--- scripts/cpu_memory_monitor.sh
+++ scripts/cpu_memory_monitor.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 
 
-CPUs=$(top -b -n 1 | grep "Cpu")
+# check for input 
+if [[ $# -eq 0 ]];then
+  echo "Usage: $0 <no. iterations>"
+  exit 1
+fi
 
-echo $CPUs
+total_cpu_usage(){
+  res=$(top -bn 2 | grep "Cpu" | tail -n 1 | awk '{print 100-$8}')
+  echo "CPU Usage: $res%"
+}
+
+total_ram_usage(){
+  free=$(free -gh | grep "Mem:" | awk '{print $7}')
+  total=$(free -gh | grep "Mem:" | awk '{print $2}') 
+  echo "Memory Usage: $free out of total $total RAM."
+}
+
+for (( i=0; i<"$1"; i++));do
+  total_cpu_usage
+  total_ram_usage
+  echo "====================================================="
+done
+
+# total = 100 - idle%
+# top -b -n 2 runs top in batch mode and get the first two iterations 
+
+
